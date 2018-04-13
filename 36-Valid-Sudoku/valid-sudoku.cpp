21ffa41a67dbbf663c5908e088d4fffc34cd5139
xta0 <jayson.xu@foxmail.com>
2018-04-13 07:46:25 -0400

    update

---
+++ 36-Valid-Sudoku/valid-sudoku.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<char> dots;
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<board.size();++i){
+            for(int j=0;j<board.size();++j){
+                if(board[i][j]=='c'){
+                    
+                }
+                
+            }
+        }
+        return false;    
+    }
+};
+
+int main(){
+
+
+
+
+    return 0;
+}
