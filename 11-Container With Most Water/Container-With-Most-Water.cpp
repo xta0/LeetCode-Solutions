602a718b2c3c58c3a29ce0e606b4df2afa2e2820
xta0 <jayson.xu@foxmail.com>
2018-04-17 07:42:32 -0400

    update

--- 11-Container With Most Water/Container-With-Most-Water.cpp
+++ 11-Container With Most Water/Container-With-Most-Water.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-
-
-        return 0;       
+        
+        //greedy
+        int start = 0,end = (int)height.size()-1;
+        int area = 0,result = 0;
+        
+        while(start != end){
+            int h1 = height[start];
+            int h2 = height[end];
+            if(h1 <= h2){
+                area = h1 * (end - start);
+                start ++;
+            }else{
+                area = h2 * (end-start);
+                end--;
+            }
+            if(area > result){
+                result = area;
+            }
+        }
+        return result;
     }
 };
 
 
 int main(){
-
-
-
-
+    
     return 0;
 }
