18906c8152b4df9fc8c7e999a747dfb6c0b9258e
xta0 <jayson.xu@foxmail.com>
2018-05-23 14:10:20 -0400

    update

--- 78-Subsets/Subsets-DFS.cpp
+++ 78-Subsets/Subsets-DFS.cpp
@@ -4,36 +4,33 @@
 using namespace std;
 
 class Solution {
-private: 
-    void dfs(vector<int>& nums, vector<int>& chosen, set<vector<int>>& result){
+private:
+    void dfs(vector<int>& nums, vector<int>& chosen, vector<vector<int>>& result){
         if(nums.size() == 0){
             return;
         }else{
-            for(int i =0; i<nums.size(); ++i){
-                int n = nums[i];
-                chosen.push_back(n);
-                result.insert(chosen);
-                nums.erase(nums.begin()+i);
-                dfs(nums,chosen,result);
-                chosen.pop_back();
-                nums.insert(nums.begin()+i, n);
-            }
+            int n = nums.back();
+            nums.pop_back();
+            chosen.push_back(n);
+            result.push_back(chosen);
+            dfs(nums,chosen, result);
+            chosen.pop_back();
+            dfs(nums,chosen,result);
+            nums.push_back(n);
         }
     }
 public:
     //subset- DFS
     vector<vector<int>> subsets(vector<int>& nums) {
-        
         vector<int> chosen;
-        set<vector<int>> sets;
-        dfs(nums,chosen,sets);
-        sets.insert({});
-
-        return {sets.begin(), sets.end()};
+        vector<vector<int>> vec;
+        dfs(nums,chosen,vec);
+        vec.push_back({});
+        return vec;
     }
 };
 int main(){
-
+    
     Solution s;
     vector<int> input = {1,2,3};
     vector<vector<int>>ret = s.subsets(input);
