688b48d31474f02bd04c143423d4904d40d71fc3
xta0 <jayson.xu@foxmail.com>
2018-05-29 12:26:22 -0400

    update

---
+++ 38-Count-and-Say/Count-and-Say.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 1){
+            return "1";
+        }
+        string str = countAndSay(n-1);
+        int sz = str.length();
+        int left = 0, right = 0, count = 0;
+        char cl;
+        string ret("");
+        while( left< sz ){
+            count = 0; cl = str[left];
+            while(str[right] == cl){
+                count ++;
+                right ++;
+                if(right >= sz){
+                    break;
+                }
+            }
+            ret += to_string(count) +  string(1,cl);
+            left += count;
+        }
+        
+        return ret;
+    }
+};
+
+int main(){
+    
+    Solution s;
+    cout<<s.countAndSay(5)<<endl;;
+    
+    
+    return 0;
+}
