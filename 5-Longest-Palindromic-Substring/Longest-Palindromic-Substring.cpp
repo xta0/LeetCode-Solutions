28e2bf0986ad792a107bc07116d94c949889a22c
xta0 <jayson.xu@foxmail.com>
2018-06-04 01:00:26 -0400

    update

--- 5-Longest-Palindromic-Substring/Longest-Palindromic-Substring.cpp
+++ 5-Longest-Palindromic-Substring/Longest-Palindromic-Substring.cpp
@@ -1,20 +1,61 @@
 #include <iostream>
-#include <stack>
+#include <string>
 #include <algorithm>    // std::max
 
 using namespace std;
 
+//中心扩散 - O(n^2)
 class Solution {
+private:
+    string helper(string& ss, int left , int right){
+        while (left >0 && right < ss.length()-1) {
+            if(ss[left-1]==ss[right+1]){
+                left --;
+                right ++;
+            }else{
+                break;
+            }
+        }
+        return ss.substr(left,right-left+1);
+    }
 public:
     string longestPalindrome(string s) {
-        
+        if(s.length() == 0 || s.length() == 1){
+            return s;
+        }
+        string last(""),ret("");
+        int k = 0;
+        for(int i=0;i<s.length(); ++i){
+            //剪枝
+            if(s.length() - 1 <= ret.length()/2){
+                 break;
+            }
+            //确定右边界
+            k = i;
+            while(s[k] == s[k+1]){
+                ++k;
+            }
+            last = helper(s,i, k);
+            if(last.length() > ret.length()){
+                ret = last;
+            }
+        }
+        return ret;
     }
 };
 
 int main(){
-
-
-
-
+    
+    Solution s;
+    cout<<s.longestPalindrome("")<<endl;
+    cout<<s.longestPalindrome("x")<<endl;
+    cout<<s.longestPalindrome("aa")<<endl;
+    cout<<s.longestPalindrome("cbbd")<<endl;
+    cout<<s.longestPalindrome("babad")<<endl;
+    cout<<s.longestPalindrome("abxba")<<endl;
+    cout<<s.longestPalindrome("abbaabba")<<endl;
+    cout<<s.longestPalindrome("abbaabxbax")<<endl;
+    
+    
     return 0;
 }
--- 647-Palindromic-Substrings/Palindromic-Substrings.cpp
+++ 647-Palindromic-Substrings/Palindromic-Substrings.cpp
@@ -1,22 +1,64 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
+/*
+ 遍历O(n^2)
+ */
 class Solution {
+private:
+    size_t count_palindromic_tring(string& ss, int pos, size_t radius){
+        int count = 0;
+        int rr = ss[pos]=='|'?1:0;
+        while (rr <= radius) {
+            int l = (int)(pos - rr);
+            if(l < 0){
+                break;
+            }
+            int r=  (int)(pos + rr);
+            if(r > ss.length()){
+                break;
+            }
+            char lc = ss[l];
+            char rc = ss[r];
+             if(lc==rc){
+                if( lc != '|' && rc != '|'){
+                    count++;
+                }
+            }else{
+                break;
+            }
+            rr+=2;
+        }
+        return count;
+    }
 public:
     int countSubstrings(string s) {
+        int ret = 0;
         //预处理
-        for(int i=0; i<s.length();i++){
-            s.insert(i,"*");
-        }        
-        cout<<s<<endl;
+        string ss = s;
+        for(int i=0;i<=s.length();++i){
+            ss.insert(2*i, "|");
+        }
+        cout<<ss<<endl;
+        vector<string> pl;
+        for(int i =0; i<ss.length(); ++i){
+            cout<<"pos: "<<ss[i]<<endl;
+            size_t r = i > ss.length()/2 ? ss.length()-i : i;
+            size_t count = count_palindromic_tring(ss,i,r);
+            cout<<"count: "<<count<<endl;
+            ret += count;
+            cout<<"total: "<<ret<<endl;
+            cout<<"-----------"<<endl;
+        }
+        return ret;
     }
-    return -1;
 };
 
 int main(){
     Solution s;
-    s.countSubstrings("abc");
-
+    s.countSubstrings("aaa");
+    
     return 0;
 }
