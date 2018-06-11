9140131658b0cba2bc63f9d071885524ea70e001
xta0 <jayson.xu@foxmail.com>
2018-06-11 08:16:32 -0400

    update

---
+++ 111-Minimum-Depth-of-Binary-Tree/Minimum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        int depth = 0;
+        queue<TreeNode* > q;
+        q.push(root);
+        while(!q.empty()){
+            depth ++;
+            int k = q.size();
+            for(int i =0; i<k; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left || node->right)
+                {
+                    if(node->left)
+                        q.push(node->left);
+                      if(node->right){
+                        q.push(node->right);
+                      }
+                }else{
+                    return depth;
+                }
+            }
+        }
+        return depth;
+    }
+};
+
+int main(){
+
+    return 0;
+}
