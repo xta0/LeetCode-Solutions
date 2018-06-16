e0e2c784de5a494fe6b6131cf80517704439131a
xta0 <jayson.xu@foxmail.com>
2018-06-16 08:53:58 -0400

    update

--- 543-Diameter-of-Binary-Tree/Diameter-of-Binary-Tree.cpp
+++ 543-Diameter-of-Binary-Tree/Diameter-of-Binary-Tree.cpp
@@ -24,15 +24,17 @@ public:
         return 1+max(depth(root->left), depth(root->right));
     }
 
+
     int diameterOfBinaryTree(TreeNode* root) {
         if(!root){return 0;}
         if(!root->left && !root->right){ return 0; }
-        //case1: 最长路径中包含根节点，则取 depth(left) + depth(right) +1
-        //case2: 最长路径中不包含根节点，则取max(depth(left),depth(right))
-        //递归操作
-        int left = depth(root->left);
-        int right = depth(root->right);
-        int ret =  max(left+right, max(left,right));
+        //每个节点的直径计算为：1(root) + depth(left) + depth(right) - 1
+        //递归计算
+
+        int rt = depth(root->left)+depth(root->right);
+        int ld = diameterOfBinaryTree(root->left);
+        int rd = diameterOfBinaryTree(root->right);
+        int ret =  max(rt, max(ld,rd));
         return ret;
 
     }
