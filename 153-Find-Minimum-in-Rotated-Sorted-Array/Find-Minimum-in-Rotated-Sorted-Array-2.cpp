5f54c90584ae72ea75fe1fe0acb19a13f5c7815f
xta0 <jayson.xu@foxmail.com>
2018-06-25 15:07:22 -0400

    update

--- 153-Find-Minimum-in-Rotated-Sorted-Array/Find-Minimum-in-Rotated-Sorted-Array-2.cpp
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/Find-Minimum-in-Rotated-Sorted-Array-2.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 //二分查找
+//模板同#33
+//二分查找 return的条件不同
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -13,30 +15,48 @@ public:
         if(nums.size() == 1){
             return nums[0];
         }
-            
-        //at least 3 elements
         //binary search
-        int left = 0; int right = nums.size()-1;
+        int left = 0; int right = (int)nums.size()-1;
+        int mid = 0;
         while(left <= right){
-            int mid = (left+right)/2;
-            if(mid == right || mid == left){
+            mid = (left+right)/2;
+            //case 1: mid = left or mid = right
+            if(mid == left || mid == right){
                 return min(nums[left], nums[right]);
             }
-            if(nums[mid] <  nums[left] ){
-                right = mid - 1;
-            }else{
-                left = mid + 1;
+            //case 2: mid is the pivot point
+            if((nums[mid] < nums[mid-1]) && (nums[mid] < nums[mid+1])){
+                return nums[mid];
             }
+            
+            //narrow down search range by compare nums[mid] and nums[left]
+            //then compare nums[left] and nums[right]
+            if(nums[mid] >=  nums[left]){
+                
+                if(nums[left] > nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid-1;
+                }
+            }else{
+                if(nums[left] > nums[right]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+            
         }
         return -1;
-
     }
 };
 
 int main(){
-
-
-
-
+    vector<int> arr = {3,1,2};
+    Solution s;
+    cout<<s.findMin(arr)<<endl;
+    
+    
+    
     return 0;
 }
