32a6e9b5bd5e61441df1ee574378fbb56f05b360
xta0 <jayson.xu@foxmail.com>
2018-07-23 17:15:41 -0400

    update

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock-BF.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// Brutoal force
+// O(n^2)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sum = 0;
+        for(int i=0;i<prices.size();i++){
+            for(int j=i+1; j<prices.size();j++){
+                if(prices[j] > prices[i]){
+                    sum = max(sum, prices[j]-prices[i]);
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+int main(){
+
+
+
+
+    return 0;
+}
