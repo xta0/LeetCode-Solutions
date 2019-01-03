0770504b4554cc4891bec0b5a8a8a7a1bfdd25aa
xta0 <jayson.xu@foxmail.com>
2019-01-03 00:00:51 -0500

    Update(auto commit)

--- 42-Trapping-Rain-Water/Trapping-Rain-Water-1.cpp
+++ 42-Trapping-Rain-Water/Trapping-Rain-Water-1.cpp
@@ -7,22 +7,20 @@
 #include <unordered_map>
 using namespace std;
 
-void log(vector<int>& v){
-    for(auto x: v){
-        cout<<x<<" ";
-    }
-    cout<<endl;
-}
 /*
  Solution: Scan 3 times
- Time: O(N)
- Space: O(1)
+ Time: O(N*3)
+ Space: O(N)
  */
 /*
- 对任意一个位置，存水量的体积 = min(左边高点，右边最高点)
+ 对任意一个位置，存水量的体积 = min(左边高点，右边最高点) - 自己的高度
+ 
+ 1. 从左向右扫一遍，计算每个位置向左边看的最高点，存到left[i]
+ 2. 从右向左扫一遍，计算每个位置向右边看的最高点，存到right[i]
+ 3. 使用上面公式计算，sum += min(left[i], right[i]) - height[i]
  */
 class Solution {
-    public:
+public:
     int trap(vector<int>& height) {
         if(height.empty()){
             return 0;
@@ -31,17 +29,22 @@ class Solution {
         vector<int> left(sz,0);
         vector<int> right(sz,0);
         
+        //计算每个位置左边看去的最高点
+        left[0] = height[0];
         for(int i=1; i<sz;i++){
-            left[i] = height[i] > height[i-1] ? left[i-1]:max(height[i-1],left[i-1]);
+            left[i] = max(left[i-1],height[i]);
         }
-        log(left);
         
+        
+        //计算每个位置右边看去的最高点
+        right[sz-1] = height[sz-1];
         for(int i=sz-2;i>=0;i--){
-            right[i] = height[i] > height[i+1]? right[i+1]:max(height[i+1],left[i+1]);
+            right[i] = max(right[i+1],height[i]);
         }
-        log(right);
+        
         int sum = 0;
         for(int i=0; i<sz;i++){
+            //计算每个位置的存水体积
             int v = min(left[i],right[i]) -height[i];
             sum += (v < 0 ? 0 : v);
         }
